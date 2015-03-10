5bc0cac01aa0a64f9ee1182f65b41034d93a552d
scottpham <scottgpham@gmail.com>
2015-03-10 15:29:12 -0700

    scaffold angular code

---
+++ js/app.js
@@ -0,0 +1,21 @@
+// declare module
+var app = angular.module("iframeApp", []);
+
+app.controller("iframeController", ["$scope", function($scope){
+
+// dummy data
+
+// dummy url
+var url = "http://scottpham.github.io/birdstrike-charts/child.html";
+// dummy height
+var height = 800;
+
+// attach to scope
+$scope.url =url;
+$scope.height = height;
+
+console.log($scope);
+
+}//end of function
+
+	]);//end of controller config 
