3e74f94d761584250aa6f38869a9f092d7b0b823
scottpham <scottgpham@gmail.com>
2015-03-10 19:35:33 -0700

    attempt

--- js/app.js
+++ js/app.js
@@ -3,19 +3,18 @@ var app = angular.module("iframeApp", []);
 
 app.controller("iframeController", ["$scope", function($scope){
 
-// dummy data
+	// dummy data
+	// dummy url
+	var url = "http://scottpham.github.io/birdstrike-charts/child.html";
+	// dummy height
+	var height = 800;
 
-// dummy url
-var url = "http://scottpham.github.io/birdstrike-charts/child.html";
-// dummy height
-var height = 800;
+	// attach to scope
+	$scope.url = url;
+	$scope.height = height;
 
-// attach to scope
-$scope.url =url;
-$scope.height = height;
+	console.log($scope);
 
-console.log($scope);
+	}//end of function
 
-}//end of function
-
-	]);//end of controller config 
+]);//end of controller config 
