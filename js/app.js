e918de1e4efcf1b05e1ec576e92034564a46f900
scottpham <scottgpham@gmail.com>
2015-03-22 17:48:45 -0700

    fixed logic, prevented loading of empty url

--- js/app.js
+++ js/app.js
@@ -4,26 +4,35 @@ var app = angular.module("iframeApp", ['ngSanitize']);
 app.controller("iframeController", ["$scope",'$sce', function($scope, $sce){
 
 	// dummy data
-	this.url = "http://scottpham.github.io/birdstrike-charts/child.html";
-	this.height = 600;
-	this.myIframe = $sce.trustAsHtml('<iframe src=' + this.url + ' scrolling="no" frameborder="0" width="622px" height=' + this.height + 'px"><a href="{{url}}">View an interactive on a separate page.</a> </iframe>');
+	this.url = "";
+	this.height;
 	this.width = 622;
-
 	this.widthPlus = +this.width + 10;
-
 	this.mobileWidth=300;
+
+	if(this.url.length>0){
+		this.myIframe = $sce.trustAsHtml('<iframe src=' + this.url + ' scrolling="no" frameborder="0" width="622px" height=' + this.height + 'px"><a href="{{url}}">View an interactive on a separate page.</a> </iframe>');
+	} else{
+		this.myIframe = "";
+	}
 	
+	
+	//pass cope to watchgroup
 	var that = this;
 
 	$scope.$watchGroup(['iframe.url', 'iframe.height', 'iframe.width'], function(newVal, oldVal){
-		
-		console.log(newVal);
 
+		// dump out if there is no newVal
 		if(!newVal) return;
+		// update widthPlus for 
+		that.widthPlus = +this.width + 10;
+		// dump out if height is zero or url is empty
+		if (!that.height || that.url.length<1) return;
 
 		that.myIframe = $sce.trustAsHtml('<iframe src=' + that.url + ' scrolling="no" frameborder="0" width="' + that.width + '" height=' + that.height + 'px"><a href="{{that.url}}">View an interactive on a separate page.</a> </iframe>');
+	
 
-		that.widthPlus = +this.width + 10;
+		
 
 	});
 
